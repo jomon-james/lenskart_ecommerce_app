daa34090a1ee0e78a9cf8bc76935f4acd54d8e93
Jomon James <jomonjames117@gmail.com>
2026-04-08 17:30:16 +0530

    server port changed

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 
 
