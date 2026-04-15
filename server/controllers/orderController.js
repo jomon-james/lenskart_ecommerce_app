df3dfbf4ec424c6c13ae939ee5e5dbb51185561d
Jomon James <jomonjames117@gmail.com>
2026-04-15 17:26:57 +0530

    success url changed

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -68,8 +68,8 @@ const createCheckoutSession =  async ( req, res) => {
             payment_method_types: ["card"],
             line_items: lineItems,
             mode: "payment",
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cancel",
+            success_url: "https://lenskart-ecommerce-app.onrender.com/success",
+            cancel_url: "https://lenskart-ecommerce-app.onrender.com/cancel",
         });
 
         res.json({ url: session.url });
