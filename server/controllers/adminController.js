ee0dc1a8f7a9141b0adb3f7b9ca97aaaa86873bf
Jomon James <jomonjames117@gmail.com>
2026-04-22 11:42:18 +0530

    small file name change in server

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,4 @@
-const Product = require("../models/Product");
+const Product = require("../models/Products");
 const Order = require("../models/Order");
 
 const getDashboardStats = async (req, res) => {
