e5642e08ed9c4a64e6764e4f8a0f2e47a60ad0f9
Jomon James <jomonjames117@gmail.com>
2026-04-29 17:43:56 +0530

    home page banners removed

--- client/src/assets/Home.jsx
+++ client/src/assets/Home.jsx
@@ -6,23 +6,6 @@ function Home() {
         <>
         <Carousel />
 
-        <div className="banner-container">
-            <div className="banner-large">
-                <img src="/images/do-more-be-more-070126.webp" alt="Large Banner" />
-            </div>
-
-            <div className="small-banners">
-                <div className="banner">
-                    <img src="/images/22-1002.webp" alt="Small Banner 1" />
-                </div>
-
-                <div className="banner">
-                    <img src="/images/Frame_2147237259-1002.webp" alt="Small Banner 2" />
-                </div>
-            </div>
-
-        </div>
-
         </>
     );
 }
