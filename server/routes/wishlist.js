fa848b6df29818f29fff1f1c551ced4f81271cc5
Jomon James <jomonjames117@gmail.com>
2026-05-06 13:05:37 +0530

    wishlist error solving step 2

--- server/routes/wishlist.js
+++ server/routes/wishlist.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../models/User");
-const Product = require("../models/Product");
+const Product = require("../models/Products");
 
 
 
